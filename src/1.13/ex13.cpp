a5ec9e505578104e09e67aab00982b10e20e651b
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-01 11:54:07 +0900

    add 1.13

---
+++ src/1.13/ex13.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  // N要素の配列
+  vector<int> A(N);
+
+  // 入力処理
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+
+  // 合計点
+  int sum = 0;
+
+  // 合計点を計算
+  for (int i = 0; i < N;i++){
+    sum += A.at(i);
+  }
+
+  // 平均点
+  int mean = sum / N;
+
+  // 平均点から何点離れているかを計算して出力
+  for (int i = 0; i < N; i++) {
+    // ②ここにプログラムを追記
+    cout << abs((A.at(i) - mean)) << endl;
+    // 負の数を出力しないように注意
+  }
+}
